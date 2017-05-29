a7878ba6bc855e27508656f33f532f69c45200c8
wharley <wharleyangelica@gmail.com>
2017-05-29 15:56:40 -0300

    Create api react todo-App

---
+++ backend/src/config/cors.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = (req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIOS, PUT, PATCH, DELETE')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	next()
+}
---
+++ backend/src/config/server.js
@@ -0,0 +1,17 @@
+'use strict'
+const port = 3006
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const allowCors = require('./cors')
+
+server.use(bodyParser.urlencoded({ extended: true}))
+server.use(bodyParser.json())
+server.use(allowCors)
+
+server.listen(port, () => {
+	console.log(`BACKEND is running on port ${port}. `)
+})
+
+module.exports = server
